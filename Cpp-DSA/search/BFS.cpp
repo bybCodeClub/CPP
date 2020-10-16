38b7dc2c38698f89dc5dd12e52c6753ea7a5bdcb
iFrogHop2Worlds <bbest12g@gmail.com>
2020-10-15 21:52:03 -0700

    BFS

--- Cpp-DSA/search/BFS.cpp
+++
@@ -1,5 +0,0 @@
-/*
-    Breadth first search is an algorithm for traversing or searching trees & graphs. 
-    Starting with a root node or some node sometimes reffered too as a search key.
-    exploring neighbor nodes first before moving onto the next level.
-*/
